862e2c5a2c9fee25fe83a731c28ef8f21c287a7b
Quentin Gros <quentingros@mac.home>
2025-06-04 16:00:13 +0200

    management of IP records in a logs folder

---
+++ src/Sniffer/logger.py
@@ -0,0 +1,17 @@
+import os
+import scapy.all as scapy
+from . import config
+
+def log_ip(ip):
+    if ip not in config.Iplist:
+        config.Iplist.add(ip)
+        config.RegisteredIpCount += 1
+        print("IP enregistrée avec succès")
+
+        try:
+            os.makedirs(os.path.dirname(config.filename), exist_ok=True)
+
+            with open(config.filename, "a") as f:
+                f.write(ip + "\n")
+        except Exception as err:
+            config.RegisteredIpErrors.append(f"Erreur d'écriture IP: {err}")
