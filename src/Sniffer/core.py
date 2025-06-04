ed141d51c73dae95b50cedbc5b78f25d129922ab
Quentin Gros <quentingros@mac.home>
2025-06-04 19:01:55 +0200

    Delete usless comment

--- src/Sniffer/core.py
+++ src/Sniffer/core.py
@@ -5,13 +5,10 @@ from .analyzer import analyze_packet
 def start_sniffer(interface_name, count, pcap_filename="logs/capture/capture.pcap"):
     print(f"\nDébut de la capture de {count} paquets sur l'interface {interface_name}...\n")
 
-    # Capture les paquets en temps réel, analyse chaque paquet
     packets = scapy.sniff(iface=interface_name, prn=analyze_packet, count=count)
 
-    # Crée le dossier de sauvegarde si nécessaire
     os.makedirs(os.path.dirname(pcap_filename), exist_ok=True)
 
-    # Sauvegarde des paquets capturés dans un fichier .pcap
     scapy.wrpcap(pcap_filename, packets)
     print(f"\nCapture enregistrée dans {pcap_filename}")
 
--- src/Sniffer/logger.py
+++ src/Sniffer/logger.py
@@ -21,29 +21,23 @@ def start_capture(interface="eth0", pcap_filename="captures/capture.pcap"):
     Démarre la capture sur l'interface donnée et enregistre tout dans un fichier .pcap
     """
 
-    # Crée le dossier si besoin
     os.makedirs(os.path.dirname(pcap_filename), exist_ok=True)
 
     print(f"Capture en cours sur {interface}... Appuyez sur Ctrl+C pour arrêter.")
 
     try:
-        # Capture illimitée (Ctrl+C pour arrêter)
         packets = scapy.sniff(iface=interface, prn=process_packet)
 
-        # À la fin de la capture, enregistre les paquets dans un fichier .pcap
         scapy.wrpcap(pcap_filename, packets)
         print(f"Capture enregistrée dans {pcap_filename}")
 
     except KeyboardInterrupt:
         print("\nCapture arrêtée par l'utilisateur.")
-        # Sauvegarder les paquets même en cas d'arrêt
         scapy.wrpcap(pcap_filename, packets)
         print(f"Capture sauvegardée dans {pcap_filename}")
 
 def process_packet(packet):
-    """
-    Traite chaque paquet : ici, extrait les IP et appelle log_ip()
-    """
+   
     if packet.haslayer(scapy.IP):
         src_ip = packet[scapy.IP].src
         log_ip(src_ip)
